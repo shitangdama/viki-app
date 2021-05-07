72bb86237175af4fd61b9be95ea89748b0cbeb63
I536596 <liang.shi01@sap.com>
2021-05-07 18:35:33 +0800

    aa_code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
+import Navigation from "./Navigation";
 import React from "react";
 import localRoutes from "./routes";
-
-import React from "react";
-
-import "./assets/css/animate.min.css";
-import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
-import "./assets/css/demo.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-
 // import remoteRoutes from "app2/routes";
-
-const routes = localRoutes
-// const routes = [...localRoutes, ...remoteRoutes]; 
+const routes = [...localRoutes];
+// const routes = [...localRoutes, ...remoteRoutes];
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>
+  <HashRouter>
+    <div>
+      <h1>App 1</h1>
+      <Navigation />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              component={route.component}
+              exact={route.exact}
+            />
+          ))}
+        </Switch>
+      </React.Suspense>
+    </div>
+  </HashRouter>
 );
 
 export default App;
